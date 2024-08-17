d5f5bf019d039ae94052bb3dba5f51466e90cf15
Mahjoob97 <mahjoobalhaj4@gmail.com>
2024-08-17 23:20:33 +0300

    created memesData file and updated the state in meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../memesData";
 
 function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImg: "",
+  });
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const imgUrl = memesArray[randomNumber].url;
+    setMeme((prevState) => ({ ...prevState, randomImg: imgUrl }));
+  }
+
   return (
     <main className="container">
-      <form>
+      <div>
         <input type="text" placeholder="Top Text ..."></input>
         <input type="text" placeholder="Bottom Text ..."></input>
-        <button type="submit">Get New Meme image 🖼</button>
-      </form>
+        <button type="submit" onClick={getMemeImage}>
+          Get New Meme image 🖼
+        </button>
+      </div>
+      <img src={meme.randomImg} alt="" />
     </main>
   );
 }
